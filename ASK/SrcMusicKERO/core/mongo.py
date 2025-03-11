35ca35028545c0bbf84790d815205c13759a4120
0114444777771 <am6887744466@gmail.com>
2025-03-11 12:22:02 +0200

    Update mongo.py

--- ASK/SrcMusicKERO/core/mongo.py
+++ ASK/SrcMusicKERO/core/mongo.py
@@ -1,14 +1,24 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
 from config import MONGO_DB_URI
+import logging
 
-from ..logging import LOGGER
+# إعداد logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
+)
 
-LOGGER("ميوزك اليــكس").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+# استخدام logging بدلاً من LOGGER
+logging.getLogger("ميوزك اليــكس").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    LOGGER("ميوزك اليــكس").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+    mongodb = _mongo_async_.myDatabase
+    logging.getLogger("ميوزك اليــكس").info("تم الاتصـال بقاعـدة البيانـات ...✓")
 except:
-    LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    logging.getLogger("ميوزك اليــكس").error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
     exit()
