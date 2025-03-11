27487a6550f0bb59b7d40ce5178d3c35909ab8c7
0114444777771 <am6887744466@gmail.com>
2025-03-11 13:57:55 +0200

    Update __init__.py

--- ASK/SrcMusicKERO/__init__.py
+++ ASK/SrcMusicKERO/__init__.py
@@ -1,17 +1,25 @@
+import logging
 from SrcMusicKERO.core.bot import Zelzaly
 from SrcMusicKERO.core.dir import dirr
 from SrcMusicKERO.core.userbot import Userbot
 from SrcMusicKERO.misc import dbb, heroku
 
-import logging
+# إعداد الـ Logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger("ميوزك اليــكس")
+
+# إعداد الكود
+LOGGER.info("جارِ إعداد المكونات...")
 
 dirr()
 dbb()
 heroku()
 
+# بدء التشغيل
 app = Zelzaly()
 userbot = Userbot()
 
+LOGGER.info("تم تحميل المكونات بنجاح.")
 
 from .platforms import *
 
@@ -22,3 +30,5 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+
+LOGGER.info("تم تهيئة الـ APIs بنجاح.")
